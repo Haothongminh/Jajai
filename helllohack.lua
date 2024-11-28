499feab6481e0619b632c951e2f4de35fa9ca17b
Haothongminh <quocthanhvlo2024@gmail.com>
2024-11-28 19:22:39 +0700

    Update helllohack.lua

--- helllohack.lua
+++ helllohack.lua
@@ -1,1 +1,7 @@
-a = "g hah llo"
+-- Tăng tốc độ chạy cho nhân vật
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+-- Tăng tốc độ chạy
+humanoid.WalkSpeed = 100 -- Giá trị mặc định là 16, bạn có thể điều chỉnh theo ý muốn
